848673fbd28bd666f62a8508a151e9e0c59690cf
See <shannkevin.see@g.msuiit.edu.ph>
2022-04-28 12:11:15 +0800

    new one

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+
+        Teacher t1 = new Teacher();
+        Student s1 = new Student();
+
+        System.out.println(t1.y);
+
+        System.out.println(s1.x);
+    }
+}
